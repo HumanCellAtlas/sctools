cba3d449cdf6dfac51026850fab6b9e44d74fc8e
Ambrose J. Carr <ambrosejcarr@gmail.com>
2018-02-23 12:47:14 -0500

    refactored, uploaded readme

--- src/sctools/metrics.py
+++ src/sctools/metrics.py
@@ -1,135 +1,211 @@
-from typing import Iterator
-from collections import Counter
+from typing import Iterator, Iterable, TextIO
+from collections import Counter, OrderedDict
+import pysam
 
 
-class CellBarcodeMetrics:
+class MetricAggregator:
+
+    _data: OrderedDict = NotImplemented
+
+    @property
+    def data(self) -> OrderedDict:
+        return self._data
+
+    def write_csv_header(self, open_fid):
+        open_fid.write(',' + ','.join(self.data.keys()) + '\n')
+
+    def write_csv_record(self, open_fid):
+        open_fid.write(','.join(self.data.values()) + '\n')
+
+    def parse_records(self, records: Iterable[pysam.AlignedSegment]) -> None:
+        """
+        this function extracts information from all the records of a molecule and adds them to the
+        parent aggregator
+        """
+        raise NotImplementedError
+
+
+class CellBarcodeMetrics(MetricAggregator):
 
     def __init__(self):
+        self._data = OrderedDict(
+            # count information
+            number_reads=None,
+            number_molecules=None,
+            # dependent metric: reads / molecule
 
-        # count information
-        self._number_reads = 0
-        self._number_molecules = 0
-        # dependent metric: reads / molecule
+            # barcode quality data
+            number_noise_reads=None,  # long polymers, N-sequences
+            number_cell_barcode_bases_above_30=None,
+            number_perfect_cell_barcodes=None,  # inv: fraction cells with errors
+            average_number_molecule_barcode_bases_above_30=None,
+            number_perfect_molecule_barcodes=None,  # inv: fraction molecules with errors
+            average_number_genomic_reads_quality_above_30=None,
 
-        # barcode quality data
-        self._number_noise_reads = 0  # long polymers, N-sequences
-        self._number_cell_barcode_bases_above_30 = 0
-        self._number_perfect_cell_barcodes = 0  # inv: fraction cells with errors
-        self._average_number_molecule_barcode_bases_above_30 = 0
-        self._number_perfect_molecule_barcodes = 0  # inv: fraction molecules with errors
-        self._average_number_genomic_reads_quality_above_30 = 0
+            # alignment locations
+            number_reads_mapped_to_UTR=None,
+            number_reads_mapped_to_transcriptome=None,
+            number_reads_mapped_intergenic=None,
+            number_reads_mapped_intronic=None,
+            number_reads_mapped_uniquely=None,
+            number_reads_mapped_too_many_loci=None,
+            number_reads_mapped_multiple=None,
+            number_reads_mapped_outside_window=None,  # reads should be within 1000 bases of UTR
+            number_duplicate_reads=None,
+            number_genes_detected=None,
 
-        # alignment locations
-        self._number_reads_mapped_to_UTR = 0
-        self._number_reads_mapped_to_transcriptome = 0
-        self._number_reads_mapped_intergenic = 0
-        self._number_reads_mapped_intronic = 0
-        self._number_reads_mapped_uniquely = 0
-        self._number_reads_mapped_too_many_loci = 0
-        self._number_reads_mapped_multiple = 0
-        self._number_reads_mapped_outside_window = 0  # reads should be within 1000 bases of UTR
-        self._number_duplicate_reads = 0
-        self._number_genes_detected = 0
+            plus_strand_reads=None,  # strand balance
 
-        self._plus_strand_reads = 0  # strand balance
+            number_spliced_reads=None,
+            number_antisense_reads=None,
 
-        self._number_spliced_reads = 0
-        self._number_antisense_reads = 0
+            average_number_fragments_per_molecule=None,
+            number_molecules_with_single_read_evidence=None,
 
-        self._average_number_fragments_per_molecule = 0
-        self._number_molecules_with_single_read_evidence = 0
+            number_molecules_mapped_to_UTR=None,
+            number_molecules_mapped_to_transcriptome=None,
+            number_molecules_mapped_intergenic=None,
+            number_molecules_mapped_intronic=None,
 
-        self._number_molecules_mapped_to_UTR = 0
-        self._number_molecules_mapped_to_transcriptome = 0
-        self._number_molecules_mapped_intergenic = 0
-        self._number_molecules_mapped_intronic = 0
+            # higher-order statistics:
+            yield_=None,
+        )
 
-        # higher-order statistics:
-        self._yield = 0
+    def parse_records(self, records: Iterable[pysam.AlignedSegment]) -> None:
+        """
+        this function extracts information from all the records of a molecule and adds them to the
+        parent aggregator
+        """
+        raise NotImplementedError  # implement this
 
 
-class GatherCellBarcodeMetrics:
+class Gene(MetricAggregator):
+
+    def __init__(self):
+        self._data = OrderedDict(
+            # read metrics
+            number_reads_mapped_to_UTR=None,
+            number_reads_mapped_to_transcriptome=None,
+            number_reads_mapped_intronic=None,
+            number_reads_mapped_uniquely=None,
+            number_reads_mapped_multiple=None,
+            number_reads_mapped_outside_window=None,  # reads should be within 1000 bases of UTR
+            number_duplicate_reads=None,
+            average_distance_from_termination_site=None,
+
+            # strand and splicing
+            plus_strand_reads=None,  # strand balance
+            number_spliced_reads=None,
+            number_antisense_reads=None,
+
+            # molecule confidence measurements
+            average_number_fragments_per_molecule=None,
+            number_molecules_with_single_read_evidence=None,
+
+            # molecule metrics
+            number_molecules_mapped_to_UTR=None,
+            number_molecules_mapped_to_transcriptome=None,
+            number_molecules_mapped_intronic=None,
+            number_distinct_mapped_UMIs=None,
+
+            # higher-order statistics:
+            yield_=None,
+        )
+
+        exon_histogram = Counter()
+
+    def parse_records(self, record: Iterable[pysam.AlignedSegment]) -> None:
+        """
+        This function extracts information from all the records of a gene and adds them to the
+        parent aggregator
+        """
+        raise NotImplementedError  # implement this
+
+
+class MetricGatherer:
 
     def __init__(self, bam_file: str):
         self._bam_file = bam_file
 
-    def iter_cells(self) -> Iterator:
+    @staticmethod
+    def group_by_tag(self, iterable: Iterable) -> Iterable:
+        pass  # function to group an iterable by a tag and produce another iterable
+
+    def generate_output_file(self) -> TextIO:
+        pass  # some kind of CSV file, for portability
+
+    def parse_bam(self):
+        raise NotImplementedError
+
+
+class GatherCellBarcodeMetrics(MetricGatherer):
+
+    # assumes file is sorted by CB then UB tags
+
+    def iter_cells(self, record_iterator: Iterator) -> Iterator:
         """
         function to iterate over all the cells of a bam file
         """
         raise NotImplementedError
 
-    def iter_molecules(self, cell) -> Iterator:
+    def iter_molecules(self, cell_record_iterator: Iterator) -> Iterator:
         """
         function to iterate over all the molecules of a cell
         """
         raise NotImplementedError
 
-    def read_bam(self):
+    def parse_bam(self):
 
+        # wasn't sure where to put histograms; not sure if I can abstract much more here
+
+        # histogram supports: cell entropy, number cell barcodes, valid barcodes
         cell_barcode_histogram = Counter()
-        # cell entropy
-        # number cell barcodes
-        # valid barcodes
 
+        # histogram supports: umi entropy, priors for downstream analysis
         molecule_barcode_histogram = Counter()
-        # umi entropy
+
+        # initialize the aggregator
+        cell_barcode_open_fid = self.generate_output_file()
 
         for cell in self.iter_cells(self._bam_file):
-
-            # maybe make all this stuff into a custom cell class with a .csv write function
-
+            aggregator = CellBarcodeMetrics()
             for molecule in self.iter_molecules(cell):
-                pass  # fill in the data described above
+                aggregator.parse_records(molecule)
+            aggregator.write_csv_record(cell_barcode_open_fid)
 
-        # post-process the histograms for the metrics they contain
-
-
-class Gene():
-
-    def __init__(self):
-
-        # read metrics
-        self._number_reads_mapped_to_UTR = 0
-        self._number_reads_mapped_to_transcriptome = 0
-        self._number_reads_mapped_intronic = 0
-        self._number_reads_mapped_uniquely = 0
-        self._number_reads_mapped_multiple = 0
-        self._number_reads_mapped_outside_window = 0  # reads should be within 1000 bases of UTR
-        self._number_duplicate_reads = 0
-        self._average_distance_from_termination_site = 0
-
-        # strand and splicing
-        self._plus_strand_reads = 0  # strand balance
-        self._number_spliced_reads = 0
-        self._number_antisense_reads = 0
-
-        # molecule confidence measurements
-        self._average_number_fragments_per_molecule = 0
-        self._number_molecules_with_single_read_evidence = 0
-
-        # molecule metrics
-        self._number_molecules_mapped_to_UTR = 0
-        self._number_molecules_mapped_to_transcriptome = 0
-        self._number_molecules_mapped_intronic = 0
-
-        # higher-order statistics:
-        self._yield = 0
-        self._exon_histogram = Counter()
+        # todo post-process the histograms for the metrics they contain
 
 
-class GatherGeneMetrics:
+class GatherGeneMetrics(MetricGatherer):
 
     # assumes file is sorted by GE tag
 
-    def __init__(self):
-        pass
-
     def iter_genes(self, bam_file) -> Iterator:
-        raise NotImplementedError
+        raise NotImplementedError  # get all records for a gene
 
-    def read_bam(self, bam_file: str):
+    def parse_bam(self):
 
-        for gene in self.iter_genes(bam_file):
-            pass  # fill in the data described above
+        gene_open_fid = self.generate_output_file()
+
+        for gene_records in self.iter_genes(self._bam_file):
+            gene = Gene()
+            gene.parse_records(gene_records)
+            gene.write_csv_record(gene_open_fid)
+
+
+class MergeMetrics:
+
+    def __init__(self, metric_files: Iterable[str]):
+        self._metric_files = metric_files
+
+    def merge_function(self):
+        raise NotImplementedError  # merge the metrics
+
+
+class MergeCellMetrics(MergeMetrics):
+    pass  # cell metrics can be concatenated
+
+
+class MergeGeneMetrics(MergeMetrics):
+    pass  # gene metrics must be averaged and merged
 
---
+++ src/sctools/metrics_readme.md
@@ -0,0 +1,61 @@
+## Overall Metric Processing
+Metric processing relies on the structure of the Optimus pipeline. Specifically, it:
+
+1. relies upon chunking the input bam files into smaller pieces
+2. relies on the guarantee of the optimus pipeline that for each cell, all reads associated with it
+are found in the same chunk
+3. Relies on sorting the (small) bam files based on different tags by first converting them to sam. 
+The distributed nature of the pipeline makes this practical.
+4. This sorting enables us to process all of the data for a molecule at one time. This is important, 
+as many metrics integrate over a molecule's data. Without sorting, a data structure must be built to 
+build up the information from the bam file, and the resulting memory footprint is 20-40gb for a 
+standard HiSeq run.  
+5. To sort, the file needs three tags: Cell barcode (CB), Molecule barcode (UB) and Gene (GE). The 
+latter tag is not, to my knowledge, in the HTSlib spec. 
+
+Despite the reliance on optimus assumptions, We will be able to release this tool as a stand alone 
+that can run on an arbitrary, properly tagged bam file, but it will rely on other sctools 
+functionality and may NOT be effectively portable across operating systems (relies on linux sort)
+
+An abbreviated WDL could be made in the future which would contain: 
+
+```
+1. SplitBamByCellBarcode
+2. scatter[CalculateMetrics]
+3. MergeMetrics
+```
+
+## Implementation: 
+
+I believe the best course of action is to generate a metrics file that aligns with our eventual 
+count matrix. This is consistent with scanpy/loom formats which use DataFrame-like objects to store
+metadata for both samples and features. It will enable the most common access patterns of comparing
+metrics across the relevant data, and more complex access patterns that fix mixed-effect models to 
+correct for metadata values. 
+
+This means we need two metrics outputs: one for Cells, and one for genes. 
+
+I've pseudo-coded three classes, each of which must be subclassed to write a Metric: 
+```
+MetricAggregator:
+  - CellMetricAggregator
+  - GeneMetricAggregator
+
+MetricGatherer:
+  - CellMetricGatherer
+  - GeneMetricGatherer
+```
+
+MetricAggregator stores the information for a unit of the relevant data (cell, gene) as records are
+being processed, and has csv writing capability so that the information for only one unit is stored
+at a time, ensuring scalability to extremely large datasets. It has a `parse_records()` function 
+which has not yet been implemented which will do most of the work
+
+MetricGatherer defines iterables to do the record grouping, the bam parsing pattern necessary, 
+and a `check_sort_order` function to verify the file is in the required order. 
+
+MergeMetrics merges multiple metric outputs from the scattered chunks. This is a trivial 
+concatenation in the case of cell metrics, and a more complex merge in the case of gene metrics. The
+`merge_metrics` function is not yet implemented, but will carry this out. 
+
+
