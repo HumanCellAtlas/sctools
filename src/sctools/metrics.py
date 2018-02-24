e4ffb9c92f7886ce74c5f16c206b3a8f7ccd4301
Ambrose J. Carr <ambrosejcarr@gmail.com>
2018-02-24 11:09:29 -0500

    intermediate commit

--- src/sctools/metrics.py
+++ src/sctools/metrics.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Iterable, TextIO
+from typing import Iterator, Iterable, TextIO, Generator, List, Tuple
 from collections import Counter, OrderedDict
 import pysam
 
@@ -17,10 +17,10 @@ class MetricAggregator:
     def write_csv_record(self, open_fid):
         open_fid.write(','.join(self.data.values()) + '\n')
 
-    def parse_records(self, records: Iterable[pysam.AlignedSegment]) -> None:
+    def parse_records(self, records: Iterable[pysam.AlignedSegment], tag: str) -> None:
         """
-        this function extracts information from all the records of a molecule and adds them to the
-        parent aggregator
+        this function extracts information from all the records of a given tag or tag combination
+        and adds them to the parent aggregator
         """
         raise NotImplementedError
 
@@ -30,48 +30,49 @@ class CellBarcodeMetrics(MetricAggregator):
     def __init__(self):
         self._data = OrderedDict(
             # count information
-            number_reads=None,
-            number_molecules=None,
+            reads=0,
+            molecules=0,
             # dependent metric: reads / molecule
 
             # barcode quality data
-            number_noise_reads=None,  # long polymers, N-sequences
-            number_cell_barcode_bases_above_30=None,
-            number_perfect_cell_barcodes=None,  # inv: fraction cells with errors
-            average_number_molecule_barcode_bases_above_30=None,
-            number_perfect_molecule_barcodes=None,  # inv: fraction molecules with errors
-            average_number_genomic_reads_quality_above_30=None,
+            noise_reads=0,  # long polymers, N-sequences
+            cell_barcode_bases_above_30=0,
+            perfect_cell_barcodes=0,  # inv: fraction cells with errors
+            average_molecule_barcode_bases_above_30=0,
+            perfect_molecule_barcodes=0,  # inv: fraction molecules with errors
+            average_genomic_reads_quality_above_30=0,
 
             # alignment locations
-            number_reads_mapped_to_UTR=None,
-            number_reads_mapped_to_transcriptome=None,
-            number_reads_mapped_intergenic=None,
-            number_reads_mapped_intronic=None,
-            number_reads_mapped_uniquely=None,
-            number_reads_mapped_too_many_loci=None,
-            number_reads_mapped_multiple=None,
-            number_reads_mapped_outside_window=None,  # reads should be within 1000 bases of UTR
-            number_duplicate_reads=None,
-            number_genes_detected=None,
+            reads_mapped_to_UTR=0,
+            reads_mapped_to_transcriptome=0,
+            reads_mapped_intergenic=0,
+            reads_mapped_intronic=0,
+            reads_mapped_uniquely=0,
+            reads_mapped_too_many_loci=0,
+            reads_mapped_multiple=0,
+            reads_mapped_outside_window=0,  # reads should be within 1000 bases of UTR
+            duplicate_reads=0,
+            genes_detected=0,
 
-            plus_strand_reads=None,  # strand balance
+            plus_strand_reads=0,  # strand balance
 
-            number_spliced_reads=None,
-            number_antisense_reads=None,
+            spliced_reads=0,
+            antisense_reads=0,
 
-            average_number_fragments_per_molecule=None,
-            number_molecules_with_single_read_evidence=None,
+            average_fragments_per_molecule=0,
+            molecules_with_single_read_evidence=0,
 
-            number_molecules_mapped_to_UTR=None,
-            number_molecules_mapped_to_transcriptome=None,
-            number_molecules_mapped_intergenic=None,
-            number_molecules_mapped_intronic=None,
-
-            # higher-order statistics:
-            yield_=None,
+            molecules_mapped_to_UTR=0,
+            molecules_mapped_to_transcriptome=0,
+            molecules_mapped_intergenic=0,
+            molecules_mapped_intronic=0,
         )
 
-    def parse_records(self, records: Iterable[pysam.AlignedSegment]) -> None:
+    def calculate_higher_order_metrics(self):
+        """calculate any metrics that depend on aggregated data"""
+        pass  # yield
+
+    def parse_records(self, records: Iterable[pysam.AlignedSegment], tag: str) -> None:
         """
         this function extracts information from all the records of a molecule and adds them to the
         parent aggregator
@@ -84,42 +85,51 @@ class Gene(MetricAggregator):
     def __init__(self):
         self._data = OrderedDict(
             # read metrics
-            number_reads_mapped_to_UTR=None,
-            number_reads_mapped_to_transcriptome=None,
-            number_reads_mapped_intronic=None,
-            number_reads_mapped_uniquely=None,
-            number_reads_mapped_multiple=None,
-            number_reads_mapped_outside_window=None,  # reads should be within 1000 bases of UTR
-            number_duplicate_reads=None,
-            average_distance_from_termination_site=None,
+            reads_unmapped=0,
+            reads_mapped_to_UTR=0,
+            reads_mapped_to_transcriptome=0,
+            reads_mapped_intronic=0,
+            reads_mapped_uniquely=0,
+            reads_mapped_multiple=0,
+            reads_mapped_outside_window=0,  # reads should be within 1000 bases of UTR
+            duplicate_reads=0,
+            average_distance_from_termination_site=0,
 
             # strand and splicing
-            plus_strand_reads=None,  # strand balance
-            number_spliced_reads=None,
-            number_antisense_reads=None,
+            plus_strand_reads=0,  # strand balance
+            spliced_reads=0,
+            antisense_reads=0,
 
             # molecule confidence measurements
-            average_number_fragments_per_molecule=None,
-            number_molecules_with_single_read_evidence=None,
+            average_fragments_per_molecule=0,
+            molecules_with_single_read_evidence=0,
+            average_read_quality=0,
 
             # molecule metrics
-            number_molecules_mapped_to_UTR=None,
-            number_molecules_mapped_to_transcriptome=None,
-            number_molecules_mapped_intronic=None,
-            number_distinct_mapped_UMIs=None,
-
-            # higher-order statistics:
-            yield_=None,
+            molecules_mapped_to_UTR=0,
+            molecules_mapped_to_transcriptome=0,
+            molecules_mapped_intronic=0,
+            distinct_mapped_UMIs=0,
         )
 
         exon_histogram = Counter()
 
-    def parse_records(self, record: Iterable[pysam.AlignedSegment]) -> None:
+    def calculate_higher_order_metrics(self):
+        """calculate any metrics that depend on aggregated data"""
+        pass  # yield
+
+    def parse_records(self, records: Iterable[pysam.AlignedSegment], tag: str) -> None:
         """
         This function extracts information from all the records of a gene and adds them to the
         parent aggregator
         """
-        pass  # no-op
+        for record in records:
+            # check if/where the record maps
+            if record.is_unmapped:
+                self.data['reads_unmapped'] += 1
+                return
+            else:
+                record.get_tag('XF')  # stopped here
 
 
 class MetricGatherer:
@@ -133,8 +143,21 @@ class MetricGatherer:
         return self._bam_file
 
     @staticmethod
-    def group_by_tag(self, tags: Iterable[str], iterable: Iterable) -> Iterable:
-        pass  # function to group an iterable by a tag and produce another iterable
+    def iter_tag_groups(tag: str, bam_iterator: Iterator[pysam.AlignedSegment]) -> Generator[List]:
+
+        # get first read and tag set
+        reads = [next(bam_iterator)]
+        current_tag = reads[0].get_tag(tag)
+
+        for alignment in bam_iterator:
+            next_tag = alignment.get_tag(tag)
+            if next_tag == current_tag:
+                reads.append(alignment)
+            else:
+                yield iter(reads), current_tag
+                reads = [alignment]
+                current_tag = next_tag
+        yield iter(reads), current_tag
 
     def generate_output_file(self) -> TextIO:
         return open(self._output_stem, 'w')
@@ -143,7 +166,7 @@ class MetricGatherer:
         raise NotImplementedError
 
 
-class GatherCellBarcodeMetrics(MetricGatherer):
+class GatherCellMetrics(MetricGatherer):
 
     # assumes file is sorted by CB then UB tags
 
@@ -170,13 +193,15 @@ class GatherCellBarcodeMetrics(MetricGatherer):
         molecule_barcode_histogram = Counter()
 
         # initialize the aggregator
-        cell_barcode_open_fid = self.generate_output_file()
+        cell_metrics = self.generate_output_file()
 
-        for cell in self.iter_cells(self._bam_file):
+        for cell, cell_tag in self.iter_tag_groups('CB', self._bam_file):
             aggregator = CellBarcodeMetrics()
-            for molecule in self.iter_molecules(cell):
+            cell_barcode_histogram[cell_tag] += 1
+            for molecule, molecule_tag in self.iter_tag_groups('UB', cell):
                 aggregator.parse_records(molecule)
-            aggregator.write_csv_record(cell_barcode_open_fid)
+                molecule_barcode_histogram[molecule_tag] += 1
+            aggregator.write_csv_record(cell_metrics)
 
         # todo post-process the histograms for the metrics they contain
 
@@ -185,14 +210,11 @@ class GatherGeneMetrics(MetricGatherer):
 
     # assumes file is sorted by GE tag
 
-    def iter_genes(self, bam_file) -> Iterator:
-        raise NotImplementedError  # get all records for a gene
-
     def parse_bam(self):
 
         gene_open_fid = self.generate_output_file()
 
-        for gene_records in self.iter_genes(self._bam_file):
+        for gene_records in self.iter_tag_groups('GE', self._bam_file):
             gene = Gene()
             gene.parse_records(gene_records)
             gene.write_csv_record(gene_open_fid)
@@ -200,8 +222,9 @@ class GatherGeneMetrics(MetricGatherer):
 
 class MergeMetrics:
 
-    def __init__(self, metric_files: Iterable[str]):
+    def __init__(self, metric_files: Iterable[str], output_file: str):
         self._metric_files = metric_files
+        self._output_file = output_file
 
     def merge_function(self):
         raise NotImplementedError  # merge the metrics
