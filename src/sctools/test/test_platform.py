273b81a2e6ea3901db5a9be256ae7794946d1cf3
Nick Barkas <nikolas.barkas@outlook.com>
2019-10-04 09:15:40 -0400

    reformat file

--- src/sctools/test/test_platform.py
+++ src/sctools/test/test_platform.py
@@ -16,31 +16,41 @@ def test_attach_barcodes():
     temp_output_bam = temp_dir_name + 'output.bam'
 
     args = [
-        "--r1", data_dir + 'test_r1.fastq',
-        "--u2", data_dir + 'test_r2.bam',
-        "--i1", data_dir + 'test_i1.fastq',
-        "--o", temp_output_bam,
-        "--sample-barcode-start-pos", "0",
-        "--sample-barcode-length", "8",
-        "--cell-barcode-start-pos", "0",
-        "--cell-barcode-length", "16",
-        "--molecule-barcode-start-pos", "16",
-        "--molecule-barcode-length", "4"
+        "--r1",
+        data_dir + 'test_r1.fastq',
+        "--u2",
+        data_dir + 'test_r2.bam',
+        "--i1",
+        data_dir + 'test_i1.fastq',
+        "--o",
+        temp_output_bam,
+        "--sample-barcode-start-pos",
+        "0",
+        "--sample-barcode-length",
+        "8",
+        "--cell-barcode-start-pos",
+        "0",
+        "--cell-barcode-length",
+        "16",
+        "--molecule-barcode-start-pos",
+        "16",
+        "--molecule-barcode-length",
+        "4",
     ]
 
     platform.BarcodePlatform.attach_barcodes(args)
 
     with pysam.AlignmentFile(temp_output_bam, "rb", check_sq=False) as samfile:
         for read in samfile:
-            tag_CR = read.get_tag('CR')
-            tag_CY = read.get_tag('CY')
-            tag_UR = read.get_tag('UR')
-            tag_UY = read.get_tag('UY')
-            tag_SR = read.get_tag('SR')
-            tag_SY = read.get_tag('SY')
-            assert len(tag_CR) == 16
-            assert len(tag_CY) == 16
-            assert len(tag_UR) == 4
-            assert len(tag_UY) == 4
-            assert len(tag_SR) == 8
-            assert len(tag_SY) == 8
+            tag_cr = read.get_tag('CR')
+            tag_cy = read.get_tag('CY')
+            tag_ur = read.get_tag('UR')
+            tag_uy = read.get_tag('UY')
+            tag_sr = read.get_tag('SR')
+            tag_sy = read.get_tag('SY')
+            assert len(tag_cr) == 16
+            assert len(tag_cy) == 16
+            assert len(tag_ur) == 4
+            assert len(tag_uy) == 4
+            assert len(tag_sr) == 8
+            assert len(tag_sy) == 8
