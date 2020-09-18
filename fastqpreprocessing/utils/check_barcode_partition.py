1a2b66e7afc22dc3071ceb6d1f4ad0fb211d77b2
Kishori Konwar <kkonwar@broadinstitute.org>
2020-09-18 14:08:17 +0000

    formatted the python

--- fastqpreprocessing/utils/check_barcode_partition.py
+++ fastqpreprocessing/utils/check_barcode_partition.py
@@ -4,7 +4,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--bam", nargs = '+', dest="bams", help = 'BAM files')
+parser.add_argument("--bam", nargs="+", dest="bams", help="BAM files")
 
 
 def check_disjoint_cbs():
@@ -14,30 +14,28 @@ def check_disjoint_cbs():
     tot_alignments = 0
 
     for bam in opts.bams:
-       print('reading ' + bam)
-       barcodes[bam] = {}
-       with pysam.AlignmentFile(bam, "rb", check_sq=False) as input_alignments:
-          for alignment in input_alignments:
-             tot_alignments += 1
-             if alignment.has_tag('CB'):
-                 barcodes[bam][alignment.get_tag('CB')] = True
-
-
+        print("reading " + bam)
+        barcodes[bam] = {}
+        with pysam.AlignmentFile(bam, "rb", check_sq=False) as input_alignments:
+            for alignment in input_alignments:
+                tot_alignments += 1
+                if alignment.has_tag("CB"):
+                    barcodes[bam][alignment.get_tag("CB")] = True
 
     for bam in opts.bams:
-       print('checking ' + bam)
-       files = set(opts.bams)
-       otherbams = files.difference(set([bam]))
-       for cb in barcodes[bam].keys():
-          for obam in otherbams:
-             if cb in barcodes[obam]:
-               print("not a partition")
-               return
+        print("checking " + bam)
+        files = set(opts.bams)
+        otherbams = files.difference(set([bam]))
+        for cb in barcodes[bam].keys():
+            for obam in otherbams:
+                if cb in barcodes[obam]:
+                    print("not a partition")
+                    return
 
     print("tot alignments : ", tot_alignments)
     print("is a partition")
-    return 
+    return
 
 
 if __name__ == "__main__":
-   check_disjoint_cbs()
+    check_disjoint_cbs()
