e3d10948202452a0acd852cbf388b7f6ae18883e
khajoue2 <fkhajoue@broadinstitute.org>
2022-09-19 19:08:11 -0400

    added a new program to filter a fastq file given a list of barcodes

--- fastqpreprocessing/src/fastq_common.h
+++ fastqpreprocessing/src/fastq_common.h
@@ -1,14 +1,34 @@
 #ifndef __SCTOOLS_FASTQPREPROCESSING_FASTQ_COMMON_H_
 #define __SCTOOLS_FASTQPREPROCESSING_FASTQ_COMMON_H_
 
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <vector>
+
 #include "FastQFile.h"
 #include "FastQStatus.h"
 #include "SamFile.h"
 #include "SamValidation.h"
 
-#include <functional>
-#include <string>
-#include <vector>
+// A pointer to a valid SamRecord waiting to be written to disk, and the index
+// of the g_read_arenas that pointer should be released to after the write.
+using PendingWrite = std::pair<SamRecord*, int>;
+
+class WriteQueue
+{
+public:
+  constexpr int kShutdown = -1;
+  PendingWrite dequeueWrite();
+  void enqueueWrite(PendingWrite write);
+  void enqueueShutdownSignal();
+private:
+  std::mutex mutex_;
+  std::condition_variable cv_;
+  std::queue<PendingWrite> queue_;
+};
 
 void fillSamRecordCommon(SamRecord* samRecord, FastQFile* fastQFileI1,
                          FastQFile* fastQFileR1, FastQFile* fastQFileR2,
@@ -20,7 +40,8 @@ void mainCommon(
     std::string white_list_file, int num_writer_threads, std::string output_format,
     std::vector<std::string> I1s, std::vector<std::string> R1s, std::vector<std::string> R2s,
     std::string sample_id,
-    std::function <void(SamRecord*, FastQFile*, FastQFile*, FastQFile*, bool)> sam_record_filler,
-    std::function <std::string(SamRecord*, FastQFile*, FastQFile*, FastQFile*, bool)> barcode_getter);
+    std::function<void(SamRecord*, FastQFile*, FastQFile*, FastQFile*, bool)> sam_record_filler,
+    std::function<std::string(SamRecord*, FastQFile*, FastQFile*, FastQFile*, bool)> barcode_getter,
+    std::function<void(WriteQueue*, SamRecord*, int)> output_handler);
 
 #endif // __SCTOOLS_FASTQPREPROCESSING_FASTQ_COMMON_H_
---
+++ fastqpreprocessing/src/samplefastq.cpp
@@ -0,0 +1,92 @@
+#include "fastq_common.h"
+#include "input_options.h"
+
+std::vector<std::pair<char, int>> parseReadStructure(std::string const& read_structure)
+{
+  std::vector<std::pair<char, int>> ret;
+  int next_ind = 0;
+  while (next_ind < read_structure.size())
+  {
+    int type_ind = read_structure.find_first_not_of("0123456789", next_ind);
+    assert(type_ind != std::string::npos);
+    char type = read_structure[type_ind];
+    int len = std::stoi(read_structure.substr(next_ind, type_ind - next_ind));
+    ret.emplace_back(type, len);
+    next_ind = type_ind + 1;
+  }
+  return ret;
+}
+
+std::vector<std::pair<char, int>> g_parsed_read_structure;
+
+void noOpFillSamRecordWithReadStructure(SamRecord* sam, FastQFile* fastQFileI1,
+                                    FastQFile* fastQFileR1, FastQFile* fastQFileR2,
+                                    bool has_I1_file_list)
+{
+
+}
+
+std::string slideseqBarcodeGetter(SamRecord* sam, FastQFile* fastQFileI1,
+                                  FastQFile* fastQFileR1, FastQFile* fastQFileR2,
+                                  bool has_I1_file_list)
+{
+  // check the sequence names matching
+  std::string a = std::string(fastQFileR1->myRawSequence.c_str());
+  // extract the raw barcode 8C18X6C9M1X
+
+  std::string barcode_seq;
+  int cur_ind = 0;
+  for (auto [tag, length] : g_parsed_read_structure)
+  {
+    switch (tag)
+    {
+    case 'C':
+      barcode_seq += a.substr(cur_ind, length);
+      break;
+    default:
+      break;
+    }
+    cur_ind += length;
+  }
+  return barcode_seq;
+}
+
+void outputHandler(WriteQueue* cur_write_queue, SamRecord* samrec, int reader_thread_index)
+{
+  cur_write_queue->enqueueWrite(std::make_pair(samrec, reader_thread_index));
+}
+
+int main(int argc, char** argv)
+{
+  INPUT_OPTIONS_FASTQ_READ_STRUCTURE options = readOptionsFastqSlideseq(argc, argv);
+  // number of output bam files, and one writer thread per bam file
+  int num_writer_threads = get_num_blocks(options);
+
+  std::ofstream outfile_r1(output_filename_r1);
+  if (!outfile_r1)
+    crash("Failed to open output file " + output_filename_r1);
+  std::ofstream outfile_r1(output_filename_r2);
+  if (!outfile_r2)
+    crash("Failed to open output file " + output_filename_r2);
+
+  g_parsed_read_structure = parseReadStructure(options.read_structure);
+  mainCommon(options.white_list_file, /*num_writer_threads=*/1, options.output_format,
+             options.I1s, options.R1s, options.R2s, options.sample_id,
+             noOpFillSamRecordWithReadStructure, slideseqBarcodeGetter,
+             [&outfile_r1, &outfile_r2](WriteQueue* ignored1, SamRecord* samrec, int ignored2)
+             {
+               // Assumed read structure of 8C18X6C9M1X with a fixed spacer sequence
+               char* barcode = sam->getString("CR").c_str();
+               char* quality_score = sam->getString("CY").c_str()
+               r1_out << "@" << sam->getReadName() << "\n"
+                      << std::string_view(barcode, 8) << "CTTCAGCGTTCCCGAGAG" << std::string_view(barcode+8, 6) << sam->getString("UR") <<"T\n"
+                      << "+\n"
+                      << std::string_view(quality_score, 8)<<"FFFFFFFFFFFFFFFFFF" << std::string_view(quality_score+8, 6) << sam->getString("UY") <<"F"<< "\n";
+
+               r2_out << "@" << sam->getReadName() << "\n"
+                      << sam->getSequence() << "\n"
+                      << "+\n"
+                      << sam->getQuality() << "\n";
+             });
+  return 0;
+}
